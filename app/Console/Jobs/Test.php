8ce94be6bdd69a17275b6221898c363ebc25c30c
falstack <icesilt@outlook.com>
2020-04-09 07:35:10 +0800

    add: migration state

--- app/Console/Jobs/Test.php
+++ app/Console/Jobs/Test.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Jobs;
 
+use App\Http\Modules\Counter\PinLikeCounter;
+use App\Http\Repositories\PinRepository;
+use App\Http\Repositories\UserRepository;
 use App\Models\Content;
+use App\Models\Pin;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -28,6 +32,48 @@ class Test extends Command
      */
     public function handle()
     {
+        $this->migrationPinLike();
+        return true;
+    }
+
+    protected function migrationPinLike()
+    {
+        $data = DB
+            ::table('followables')
+            ->where('migration_state', 0)
+            ->where('followable_type', 'App\Models\Pin')
+            ->where('relation', 'upvote')
+            ->get()
+            ->toArray();
+
+        if (empty($data))
+        {
+            return false;
+        }
+
+        $pinLikeCounter = new PinLikeCounter();
+        foreach ($data as $row)
+        {
+            $authorSlug = Pin::where('id', $row->followable_id)->pluck('user_slug')->first();
+
+            $pinLikeCounter->set(
+                $row->user_id,
+                $row->followable_id,
+                slug2id($authorSlug),
+                1,
+                $row->created_at
+            );
+
+            DB::table('followables')
+                ->where('followable_type', 'App\Models\Pin')
+                ->where('relation', 'upvote')
+                ->where('user_id', $row->user_id)
+                ->where('followable_id', $row->followable_id)
+                ->update([
+                    'migration_state' => 1
+                ]);
+        }
+
         return true;
     }
 }
--- app/Http/Modules/Counter/UserFollowCounter.php
+++ app/Http/Modules/Counter/UserFollowCounter.php
@@ -6,10 +6,10 @@ namespace App\Http\Modules\Counter;
 
 use App\Http\Modules\Counter\Base\SocialCounter;
 
-class PinRewardCounter extends SocialCounter
+class UserFollowCounter extends SocialCounter
 {
     public function __construct()
     {
-        parent::__construct('pin_reward_counter');
+        parent::__construct('user_follow_counter');
     }
 }
