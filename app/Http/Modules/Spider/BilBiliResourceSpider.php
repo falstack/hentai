ad171147d8e6bcd56854545afca478ae441de61b
falstack <icesilt@outlook.com>
2020-05-04 08:45:20 +0800

    save

--- app/Http/Modules/Spider/BilBiliResourceSpider.php
+++ app/Http/Modules/Spider/BilBiliResourceSpider.php
@@ -20,11 +20,13 @@ class BilBiliResourceSpider extends GetResourceService
 
     public function getDataItem($id, $type, $source)
     {
-        $data = $this->getClient("https://api.bilibili.com/x/web-interface/view/detail?bvid={$id}");
+        $data = $this->getClient("https://api.bilibili.com/x/web-interface/view/detail?aid={$id}");
         $row = $data['data']['View'];
         if ($source === null)
         {
             return [
+                'avid' => $row['aid'],
+                'bvid' => $row['bvid'],
                 'username' => $row['owner']['name'],
                 'poster' => $this->getImage($row['pic']),
                 'title' => $row['title'],
@@ -37,6 +39,8 @@ class BilBiliResourceSpider extends GetResourceService
         }
 
         return array_merge($source, [
+            'avid' => $row['aid'],
+            'bvid' => $row['bvid'],
             'visit_count' => $row['stat']['view'],
             'comment_count' => $row['stat']['reply'],
             'danmu_count' => $row['stat']['danmaku']
@@ -45,7 +49,30 @@ class BilBiliResourceSpider extends GetResourceService
 
     public function getUserList($id, $rule)
     {
-        // TODO 根据rule做数据获取
+        if (!$rule)
+        {
+            return $this->getDefaultList($id);
+        }
+
+        $result = [];
+        foreach ($rule as $row)
+        {
+            if ($row['type'] === 'video')
+            {
+                foreach ($row['channel_id'] as $cid)
+                {
+                    $data = $this->getChannelVideo($id, $cid, $row['source_type']);
+
+                    $result = array_merge($result, $data);
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    protected function getDefaultList($id)
+    {
         $data = $this->getClient("https://api.bilibili.com/x/space/arc/search?mid={$id}&pn=1&ps=100");
         $list = $data['data']['list']['vlist'];
 
@@ -62,9 +89,11 @@ class BilBiliResourceSpider extends GetResourceService
                 'source_type' => 0,
                 'site_type' => 1,
                 'model_type' => 1,
-                'model_id' => $row['bvid'],
+                'model_id' => $row['aid'],
                 'author_id' => $row['mid'],
                 'data' => json_encode([
+                    'avid' => $row['aid'],
+                    'bvid' => $row['bvid'],
                     'username' => $row['author'],
                     'poster' => $this->getImage($row['pic']),
                     'title' => $row['title'],
@@ -73,6 +102,42 @@ class BilBiliResourceSpider extends GetResourceService
                     'danmu_count' => 0,
                     'published_at' => $row['created'],
                     'duration' => $this->formatDurationStr($row['length'])
+                ]),
+                'created_at' => $now,
+                'updated_at' => $now
+            ];
+        }
+
+        return $result;
+    }
+
+    protected function getChannelVideo($uid, $cid, $sourceType)
+    {
+        $data = $this->getClient("https://api.bilibili.com/x/space/channel/video?mid={$uid}&cid={$cid}&pn=1&ps=100&order=0");
+
+        $list = $data['data']['list']['archives'];
+
+        $result = [];
+        $now = Carbon::now();
+        foreach ($list as $row)
+        {
+            $result[] = [
+                'source_type' => $sourceType,
+                'site_type' => 1,
+                'model_type' => 1,
+                'model_id' => $row['aid'],
+                'author_id' => $uid,
+                'data' => json_encode([
+                    'avid' => $row['aid'],
+                    'bvid' => '',
+                    'username' => $row['owner']['name'],
+                    'poster' => $this->getImage($row['pic']),
+                    'title' => $row['title'],
+                    'visit_count' => $row['stat']['view'],
+                    'comment_count' => $row['stat']['reply'],
+                    'danmu_count' => $row['stat']['danmaku'],
+                    'published_at' => $row['pubdate'],
+                    'duration' => $row['duration']
                 ]),
                 'created_at' => $now,
                 'updated_at' => $now
