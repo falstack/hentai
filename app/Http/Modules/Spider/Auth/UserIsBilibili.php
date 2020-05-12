e75365fd80c558417abb6778202e9487fcefe002
falstack <icesilt@outlook.com>
2020-05-12 09:39:21 +0800

    fixed: typo

--- app/Http/Modules/Spider/Auth/UserIsBilibili.php
+++ app/Http/Modules/Spider/Auth/UserIsBilibili.php
@@ -53,6 +53,18 @@ class UserIsBilibili extends UserAuthLink
 
     public function status()
     {
+        try
+        {
+            $url = 'https://api.vc.bilibili.com/session_svr/v1/session_svr/get_sessions?session_type=1&group_fold=1&unfollow_fold=0&sort_rule=2&build=0&mobi_app=web';
+            $resp = $this->client->get($url, $this->headers);
+            $body = json_decode($resp->body, true);
+            $list = $body['data']['session_list'];
 
+            return gettype($list) === 'array';
+        }
+        catch (\Exception $e)
+        {
+            return false;
+        }
     }
 }
