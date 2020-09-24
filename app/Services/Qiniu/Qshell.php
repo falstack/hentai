c13d16182c02103b3a4551007dddb5207513c310
falstack <icesilt@outlook.com>
2020-09-24 08:08:00 +0800

    add: create audio

--- app/Services/Qiniu/Qshell.php
+++ app/Services/Qiniu/Qshell.php
@@ -10,6 +10,7 @@ namespace App\Services\Qiniu;
 
 
 use App\Services\Qiniu\Storage\BucketManager;
+use App\Services\Qiniu\Storage\UploadManager;
 
 class Qshell
 {
@@ -48,36 +49,27 @@ class Qshell
     }
 
     // 抓取音频
-    public function audio($srcResUrl, $fileName = '')
+    public function audio($source)
     {
-        if (strpos($srcResUrl, '//') === 0)
-        {
-            $srcResUrl = "http:{$srcResUrl}";
-        }
         $auth = new \App\Services\Qiniu\Auth();
-        $bucketManager = new BucketManager($auth);
+        $timeout = 3600;
+        $uptoken = $auth->uploadToken(null, $timeout, [
+            'returnBody' => '{
+                "code": 0,
+                "data": {
+                    "mime": "$(mimeType)",
+                    "size": $(fsize),
+                    "url": "$(key)",
+                    "meta": $(avinfo)
+                }
+            }'
+        ]);
 
         $now = time();
         $str = str_rand();
-        $tail = explode('?', $srcResUrl)[0];
-        $tail = explode('.', $tail);
-        if (count($tail) >= 2)
-        {
-            $tail = "." . last($tail);
-        }
-        else
-        {
-            $tail = '';
-        }
-        $target = $fileName ? $fileName : "fetch/{$now}/{$str}{$tail}";
+        $uploadManager = new UploadManager();
+        $res = $uploadManager->put($uptoken, "audio/{$now}/{$str}", $source);
 
-        list($ret, $err) = $bucketManager->fetch($srcResUrl, config('app.qiniu.bucket'), $target);
-
-        if ($err !== null)
-        {
-            return $err;
-        }
-
-        return $ret;
+        return $res;
     }
 }
